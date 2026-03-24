6d0c8203f4c6bdf60dd118aa27477443c216c92b
emanuel8006 <emanuelgalindog12@gmail.com>
2026-03-24 15:11:18 -0400

    Added usage of location service. Changed parameters for better usage

--- backend/app/services/dineoncampus_service.py
+++ backend/app/services/dineoncampus_service.py
@@ -9,7 +9,7 @@ from typing import Dict, Any, List
 from cachetools import TTLCache
 import httpx
 
-menu_cache: TTLCache = TTLCache(maxsize=100, ttl=600) # Cache with a time-to-live of 10 minutes
+menu_cache: TTLCache = TTLCache(maxsize=100, ttl=3600) # Cache with a time-to-live of 60 minutes
 
 STEAST_LOC_ID = "586d05e4ee596f6e6c04b527" # Stetson East dining hall Location ID
 IV_LOC_ID = "5f4f8a425e42ad17329be131" # International Village dining hall Location ID
@@ -32,18 +32,33 @@ HEADERS = {
 
 }
 
-async def get_menu(location_id: str, period_id: str) -> dict[str, Any]:
+async def get_menu(location_name: str, period_name: str) -> dict[str, Any]:
     """
     Gets the menu for a specific location and date from the dineoncampus API.
     Args:
-         location_id (str): The ID of the location to get the menu for.
-         period_id (str): The ID of the period to get the menu for (e.g., Breakfast, Lunch, Dinner).
+         location_name (str): The name of the location to get the menu for.
+         period_name (str): The name of the period to get the menu for (e.g., Breakfast, Lunch, Dinner).
     Returns:
         dict: the menu data as a JSON object that containes stations and items.
                 On failure, returns a JSON object with an error message and the status code.
     """
-    cache_key = f"{location_id}-{period_id}-{get_date()}"
+    # Import here to avoid cirvular imports between this file and location_service.py
+    from backend.app.services.location_service import get_location_id, get_period_id_by_name
 
+    location_id = get_location_id(location_name=location_name) # Location name -> Location ID
+    if not location_id:
+        return {
+            "error": f"Unrecognized location name: {location_name}",
+            "known_locations": ["stetson east", "international village"]
+        }
+    period_id = await get_period_id_by_name(location_id, period_name) # Period name -> Period ID
+    if not period_id:
+        return {
+            "error": f"Period name {period_name} not found for today at {location_name}",
+            "hint": "The menu may not have been published yet, or the period name may be mispelled."
+        }
+
+    cache_key = f"{location_id}-{period_id}-{get_date()}"
     if cache_key in menu_cache:
         return menu_cache[cache_key]
 
@@ -70,13 +85,14 @@ async def get_menu(location_id: str, period_id: str) -> dict[str, Any]:
                 "detail" : str(e)
             }
 
-    data = res.json()
-    print("RAW API RESPONSE:", data)
+    data: Dict[str, Any] = res.json()
+    #print("RAW API RESPONSE:", data)
     per_data = data.get("period") or {}
     if not per_data:
         return {
             "error": "No period data found in the response", "raw" : data
         }
+
     categories = per_data.get("categories", [])
     if not categories:
         return {
@@ -86,7 +102,7 @@ async def get_menu(location_id: str, period_id: str) -> dict[str, Any]:
         }
 
     stations: List[Dict[str, Any]] = [] # Holds the stations and their menu items
-
+    # Parsing the stations and menu items from API response
     for category in categories:
         # Holds the name of the station, default is unknown
         station_name = category.get("name", "Unknown Station")

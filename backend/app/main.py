278056e0ac8d30b60e81182d796d1d17de798687
emanuel8006 <emanuelgalindog12@gmail.com>
2026-04-06 10:30:05 -0400

    temp, may or may not add rate limiting

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 from app.routers import menu, auth, user, vendors
 
 app = FastAPI()
+limiter = Limiter(key_func=get_remote_address)
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 
 app.add_middleware(
     CORSMiddleware,
